39a84739e83a12018a7af7639186f759eead11a6
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-06-13 14:23:25 +0900

    DB연동

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+const app = express();
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true, useUnifiedTopology:true})
+
+const db = mongoose.connection;
+db.on('error', function(err){
+    console.log()
+})
+db.once('open', function(){
+    console.log("디비연결")
+})
+//morgan HTTP request 기록해주는 미들웨어
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 3333;
+app.listen(PORT);
